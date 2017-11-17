47387568ec8e27f530b694a4deeb499a5925a102
bgmsa <sanandres4a@gmail.com>
2017-11-17 18:23:25 +0100

    Cambios

--- src/Paciente.java
+++ src/Paciente.java
@@ -3,17 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.util.*;
+import java.math.*;
 /**
  *
  * @author Bea
  */
 public class Paciente {
     
-    private int dni;
+    private String dni;
+    private java.math.BigDecimal num_segsocial;
+    private java.math.BigDecimal num_paciente;
+    private String nombre;
+    private String direccion;
+    private java.math.BigDecimal codigo_postal;
+    private String ciudad;
+    private String provincia;
+    private java.math.BigDecimal telefono;
+    private java.math.BigDecimal num_historia_clinica;
     
-    public Paciente(int dni){
-        this.dni=dni;
+    
+    
+    public Paciente(int dni,String vuelta){
+        this.dni=dni+vuelta;
+        this.num_segsocial=dni;
+        
+        
     }
     
 }
